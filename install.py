0a68f53277ca1550e75f6b43cfcedab699fd2d1c
Jay Rajput <jayrajput@gmail.com>
2014-09-06 11:38:36 +0530

    Logging enhancements

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import os;
+import os
+import logging
 
 gitRepo = os.path.dirname(os.path.realpath(__file__))
 home    = os.path.expanduser("~")
@@ -28,19 +29,19 @@ for target in links.keys():
         # Target path not existing is a bug, but I have made mistake in past
         # where I delete the target but forgot to update the entry here.
         if not os.path.exists(target):
-            print "ERROR: Target does not exist %s" % (target)
+            logging.error("Target does not exist %s" % (target))
             continue;
 
         if os.path.islink(link):
-            print "WARN: Link already exist %s" % (link)
+            logging.warn("WARN: Link already exist %s" % (link))
             continue;
 
         # we already checked for symlink, so this is a real file, back it up
         if os.path.exists(link):
             os.rename(link, link + ".bkp")
-            print "INFO: Backed up %s -> %s" % (link, link + ".bkp")
+            logging.info("Backed up %s -> %s" % (link, link + ".bkp"))
 
         os.symlink(target, link)
-        print "INFO: Linked %s -> %s" % (link, target)
+        logging.info("Linked %s -> %s" % (link, target))
     except OSError:
-        print "ERROR: Failure running command link:%s, target:%s" % (link, target)
+        logging.error("Failure running command link:%s, target:%s" % (link, target))
