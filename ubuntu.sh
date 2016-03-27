099960fb0df304f3975f63dcc0353d89720d6b78
Jay Rajput <jayrajput@gmail.com>
2016-03-27 11:41:56 -0400

    adding pink noise command

--- ubuntu.sh
+++ ubuntu.sh
@@ -14,4 +14,9 @@ function pathpretty()
     echo $PATH | tr ":" "\n"
 }
 
+function pinknoise()
+{
+    play -c 2 -n synth pinknoise band -n 2500 4000 tremolo 0.03 5 reverb 20 gain -l
+}
+
 alias ll='ls -l'
